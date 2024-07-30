ec5e40c9d6b6d41dcf5e33cbe697a7e73faac01e
ASUS <idristamdogan@gmail.com>
2024-07-30 14:17:55 +0300

    14:17

---
+++ EmployeeService/src/main/java/com/java14/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.java14.controller;
+
+import com.java14.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+}
