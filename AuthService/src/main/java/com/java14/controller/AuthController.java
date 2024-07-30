dea3b8452a0bf957fbfb9c899cf9c406a3b703ac
aslihanbetul <aslihan.mert19@gmail.com>
2024-07-30 14:34:06 +0300

    Auth Service Entity Repository service exception ve controller oluşturuldu

--- AuthService/src/main/java/com/java14/controller/AuthController.java
+++ AuthService/src/main/java/com/java14/controller/AuthController.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    //düzenle
 }
