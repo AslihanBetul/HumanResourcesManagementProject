4576a883a0fa6141e5ecf853033226b75411f564
guner <serkan.guner@yahoo.com>
2024-07-30 12:52:36 +0300

    Initial commit

---
+++ src/main/java/com/java14/humanresourcesmanagementproject/HumanResourcesManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java14.humanresourcesmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesManagementProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesManagementProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java14/humanresourcesmanagementproject/HumanResourcesManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java14.humanresourcesmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesManagementProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
