fbc764026a905d300427641cbd23079ed4beaf87
aslihanbetul <aslihan.mert19@gmail.com>
2024-07-30 14:10:34 +0300

    Admin Service Entity Repository service exception ve controller oluşturuldu

---
+++ AdminService/src/main/java/com/java14/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.java14.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Data
+public class BaseEntity {
+}
