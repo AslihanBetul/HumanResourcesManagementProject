27046aec83983cde2d304561067d07844073dbbc
guner <serkan.guner@yahoo.com>
2024-08-01 10:07:44 +0300

    Auth security added

--- AuthService/src/main/java/com/java14/repository/AuthRepository.java
+++ AuthService/src/main/java/com/java14/repository/AuthRepository.java
@@ -3,6 +3,10 @@ package com.java14.repository;
 import com.java14.entity.Auth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository <Auth ,Long >{
     boolean existsByEmail(String email);
+
+    Optional<Auth> findOptionalById(Long authId);
 }
