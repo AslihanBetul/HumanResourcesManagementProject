72720a77c5f65712300010b2a3f33edd551136e4
aslihanbetul <aslihan.mert19@gmail.com>
2024-08-05 14:04:50 +0300

    Endpoint düzenleme

--- AdminService/src/main/java/com/java14/constants/EndPoint.java
+++ AdminService/src/main/java/com/java14/constants/EndPoint.java
@@ -3,13 +3,15 @@ package com.java14.constants;
 public class EndPoint {
 
     private static final String VERSION ="/v1";
-    private static final String DEV="/dev";
+    private static final String API="/api";
 
-    private static final String ROOT= DEV + VERSION;
+    private static final String ROOT= API + VERSION;
     public static final String ADMIN = ROOT + "/admin";
     public static final String APPROVE_MANAGER = ROOT + "/approve-manager";
     public static final String ADD= "/add";
     public static final String GET_ALL= "/get-all";
     public static final String UPDATE_ADMIN= "/update-admin";
+    public static final String CONFIRM_MANAGER = ROOT + "/   confirmManager";
+    public static final String VERIFY_EMAIL = ROOT + "/  /verifyEmail";
 
 }
